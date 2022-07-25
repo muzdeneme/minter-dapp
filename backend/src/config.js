f8a61a97fcf9064ece4ec8b4f41d7a1b72b9574b
muzdeneme <109990575+muzdeneme@users.noreply.github.com>
2022-07-25 23:26:23 +0300

    mint price

--- backend/src/config.js
+++ backend/src/config.js
@@ -54,7 +54,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xD4200305fc6f710027933525107aa78a8F4DD99E';
 const TREASURY_ADDRESS = '0xD4200305fc6f710027933525107aa78a8F4DD99E';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
